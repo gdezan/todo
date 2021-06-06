2c3763fc34593388ad2e8c7f7eb539c568a7cdff
gdezan <33992304+gdezan@users.noreply.github.com>
2021-06-06 01:24:25 -0300

    with controller

---
+++ server/src/controllers/todo.ts
@@ -0,0 +1,46 @@
+import { DeleteResult, getRepository } from "typeorm";
+import { ToDo } from "../models/todo";
+
+interface TodoPayload {
+  title?: string;
+  description?: string;
+  completed: boolean;
+}
+
+export default class TodoController {
+  public static async createTodo(todo: TodoPayload): Promise<ToDo> {
+    const todoRepository = getRepository(ToDo);
+    const newTodo = todoRepository.create(todo);
+    return await todoRepository.save(newTodo);
+  }
+
+  public static async getTodo(id: string): Promise<ToDo | undefined> {
+    const todoRepository = getRepository(ToDo);
+    return await todoRepository.findOne(id);
+  }
+
+  public static async getTodos(): Promise<Array<ToDo>> {
+    const todoRepository = getRepository(ToDo);
+    return await todoRepository.find();
+  }
+
+  public static async updateTodo(
+    id: string,
+    changes: TodoPayload
+  ): Promise<ToDo | null> {
+    const todoRepository = getRepository(ToDo);
+    const todo = await todoRepository.findOne(id);
+
+    if (!todo) {
+      return null;
+    }
+
+    todoRepository.merge(todo, changes);
+    return await todoRepository.save(todo);
+  }
+
+  public static async deleteTodo(id: string): Promise<DeleteResult> {
+    const todoRepository = getRepository(ToDo);
+    return await todoRepository.delete(id);
+  }
+}
